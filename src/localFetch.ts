252b6dae61a30f0b7f42bcb11e8ac5d36240a888
Hugo Arregui <hugo@decentraland.org>
2023-07-27 16:24:33 -0300

    linter

--- src/localFetch.ts
+++ src/localFetch.ts
@@ -1,11 +1,11 @@
-import nodeFetch, { RequestInfo, RequestInit } from "node-fetch"
+import nodeFetch, { RequestInfo, RequestInit } from 'node-fetch'
 
-import { IFetchComponent } from "@well-known-components/http-server"
-import { IConfigComponent } from "@well-known-components/interfaces"
+import { IFetchComponent } from '@well-known-components/http-server'
+import { IConfigComponent } from '@well-known-components/interfaces'
 
 // start TCP port for listeners
 /* istanbul ignore next */
-let lastUsedPort = 19000 + parseInt(process.env.JEST_WORKER_ID || "1") * 1000
+let lastUsedPort = 19000 + parseInt(process.env.JEST_WORKER_ID || '1') * 1000
 
 function getPort() {
   lastUsedPort += 1
@@ -15,7 +15,7 @@ function getPort() {
 /**
  * Default Server config
  * @public
-**/
+ **/
 export const defaultServerConfig = () => ({
   HTTP_SERVER_HOST: '0.0.0.0',
   HTTP_SERVER_PORT: String(getPort())
@@ -24,22 +24,20 @@ export const defaultServerConfig = () => ({
 /**
  * Local Fetch component for testing local urls
  * @public
-**/
-export async function createLocalFetchCompoment(
-  configComponent: IConfigComponent
-): Promise<IFetchComponent> {
+ **/
+export async function createLocalFetchCompoment(configComponent: IConfigComponent): Promise<IFetchComponent> {
   const protocolHostAndProtocol = `http://${await configComponent.requireString(
-    "HTTP_SERVER_HOST"
-  )}:${await configComponent.requireNumber("HTTP_SERVER_PORT")}`
+    'HTTP_SERVER_HOST'
+  )}:${await configComponent.requireNumber('HTTP_SERVER_PORT')}`
   // test fetch, to hit our local server
   const localFetch: IFetchComponent = {
     async fetch(url: RequestInfo, initRequest?: RequestInit) {
-      if (typeof url == "string" && url.startsWith("/")) {
+      if (typeof url === 'string' && url.startsWith('/')) {
         return nodeFetch(protocolHostAndProtocol + url, { ...initRequest })
       } else {
-        throw new Error("localFetch only works for local testing-URLs")
+        throw new Error('localFetch only works for local testing-URLs')
       }
-    },
+    }
   }
   return localFetch
 }
