282bc256130dd6615f91a38d28acb875ef9bd214
Hugo Arregui <hugo.arregui@mykolab.com>
2024-09-10 10:32:01 -0300

    Save Tue Sep 10 10:32:01 AM -03 2024

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json'
-    }
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", {tsconfig: "test/tsconfig.json"}]
   },
   moduleFileExtensions: ['ts', 'js'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
   testMatch: ['**/*.spec.(ts)'],
   testEnvironment: 'node'
 }
